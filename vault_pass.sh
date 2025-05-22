6484a5ab9b0cbfd99fa68a0742a692dad56c5b17
Felix Boerner <ich@felix-boerner.de>
2025-05-22 11:25:10 +0200

    feat: improve vault pass script

--- vault_pass.sh
+++ vault_pass.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e
 
-if [ -f "/etc/ansible/.vault_pass.txt" ]; then
+if [[ -f "/etc/ansible/.vault_pass.txt" ]]; then
   cat "/etc/ansible/.vault_pass.txt"
-elif [ -f "./.vault_pass.txt" ]; then
+elif [[ -f "./.vault_pass.txt" ]]; then
   cat "./.vault_pass.txt"
+elif [[ -n "${VAULT_PASS}" ]]; then
+  echo "${VAULT_PASS}"
 else
   exit 1
 fi
